ec7c861bf9457c06118967155682571ba18fb01d
Emmanuele Villa <emmanuele.villa@finconsgroup.com>
2023-11-29 14:46:39 +0100

    widget update

--- app/src/main/java/com/finconsgroup/midgard/renshuuwidget/WidgetSmallRow.kt
+++ app/src/main/java/com/finconsgroup/midgard/renshuuwidget/WidgetSmallRow.kt
@@ -3,18 +3,24 @@ package com.finconsgroup.midgard.renshuuwidget
 import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.datastore.preferences.core.Preferences
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.action.actionStartActivity
 import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.lazy.LazyColumn
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
 import androidx.glance.currentState
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Column
+import androidx.glance.layout.Row
 import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.fillMaxWidth
+import androidx.glance.layout.height
+import androidx.glance.layout.padding
 import androidx.glance.state.GlanceStateDefinition
 import androidx.glance.state.PreferencesGlanceStateDefinition
 import androidx.glance.text.FontStyle
@@ -44,29 +50,35 @@ class WidgetSmallRow : GlanceAppWidget() {
     @Composable
     fun Report(json: String) {
         val deserialized: List<Entry> = Gson().fromJson(json, Array<Entry>::class.java).toList()
-        val firstTitle = deserialized[0].title
-        val firstToLearn = deserialized[0].toLearn
-        val firstToReview = deserialized[0].toReview
-        val totLearn = deserialized.sumOf { it.toLearn }
-        val totReview = deserialized.sumOf { it.toReview }
-        Column(
+        LazyColumn(
             modifier = GlanceModifier
                 .fillMaxSize()
                 .clickable(onClick = actionStartActivity(MainActivity::class.java))
                 .background(Color.Gray),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(text = "$firstTitle", style = TextStyle(fontWeight = FontWeight.Bold))
-            Text(
-                text = "Learn: $firstToLearn - Review: $firstToReview",
-                style = TextStyle(fontStyle = FontStyle.Italic)
-            )
-            Text(text = "Remaining", style = TextStyle(fontWeight = FontWeight.Bold))
-            Text(
-                text = "Learn: $totLearn - Review: $totReview",
-                style = TextStyle(fontStyle = FontStyle.Italic)
-            )
+            items(deserialized.size) {
+                val value = deserialized[it]
+                Column(
+                    modifier = GlanceModifier
+                        .fillMaxWidth()
+                        .padding(2.dp)
+                        .clickable(onClick = actionStartActivity(MainActivity::class.java)),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Text(text = value.title, style = TextStyle(fontWeight = FontWeight.Bold))
+                    Text(
+                        text = "Learn: ${value.toLearn} - Review: ${value.toReview}",
+                        style = TextStyle(fontStyle = FontStyle.Italic)
+                    )
+                    if (it < deserialized.size - 1) {
+                        Row(
+                            GlanceModifier.fillMaxWidth().padding(2.dp).height(1.dp)
+                                .background(Color.DarkGray)
+                        ) {}
+                    }
+                }
+            }
         }
     }
 }
