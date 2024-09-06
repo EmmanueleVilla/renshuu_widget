4de4feb40e006423c5ba9ca97144080800cd6c6b
Emmanuele <emmanuele.villa89@gmail.com>
2024-09-06 13:44:39 +0200

    logs in debug

--- app/src/main/java/com/shadowings/renshuuwidget/refresh/WidgetRefreshWorker.kt
+++ app/src/main/java/com/shadowings/renshuuwidget/refresh/WidgetRefreshWorker.kt
@@ -4,13 +4,16 @@ import android.content.Context
 import androidx.glance.appwidget.GlanceAppWidgetManager
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.shadowings.renshuuwidget.utils.logIfDebug
 import com.shadowings.renshuuwidget.widget.WidgetSmallRowComponent
 
 class WidgetRefreshWorker(private val ctx: Context, params: WorkerParameters) :
     CoroutineWorker(ctx, params) {
     override suspend fun doWork(): Result {
+        logIfDebug("[WidgetRefreshWorker] refreshing widgets")
         GlanceAppWidgetManager(ctx).getGlanceIds(WidgetSmallRowComponent::class.java)
             .forEach { id ->
+                logIfDebug("[WidgetRefreshWorker] refreshing widget: $id")
                 refreshWidget(ctx, id)
             }
         return Result.success()
